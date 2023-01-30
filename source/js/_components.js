317dbfc6b9ae3fb5e2a51533691eebe366abf532
roman <romasaab@gmail.com>
2023-01-30 14:28:10 +0200

    adaptive main

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,12 +1,3 @@
-// import './components/accordions';
-// import './components/anchor';
-// import './components/lift';
-// import './components/galery';
-// import './components/modals';
-// import './components/sliders';
 import './components/replaceEL';
-// import './components/getDinamicHeight';
-// import './components/lightbox';
-// import './components/select';
-// import './components/tabs';
+import './components/mainSwap';
 
---
+++ source/js/components/mainSwap.js
@@ -0,0 +1,7 @@
+const elem = document.querySelector('.index-swap__wrapper');
+
+if(elem){
+elem.addEventListener('click', function(){
+    console.log('click');
+})
+}
